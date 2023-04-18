b0d0466820b7201249907e054b012494189bc036
Juan Apolinar <apolinarmejia.2@wright.edu>
2023-04-18 02:27:27 +0000

    update

--- restart.sh
+++ restart.sh
@@ -2,5 +2,7 @@
 
 sudo docker stop site
 sudo docker rm site
+sudo docker ps -a
 sudo docker pull juanapolinar001/my-website3:latest
 sudo docker run -d -p 80:80 --name site juanapolinar001/my-website3:latest
+sudo docker ps -a
