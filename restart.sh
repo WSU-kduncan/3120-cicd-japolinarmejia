a156c8ac63140df1d393681686c3979f6a15fb96
japolinarmejia <112422037+japolinarmejia@users.noreply.github.com>
2023-04-17 22:02:59 -0400

    final

---
+++ restart.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+sudo docker stop site
+sudo docker rm site
+sudo docker pull juanapolinar001/my-website3:latest
+sudo docker run -d -p 80:80 --name site juanapolinar001/my-website3:latest
